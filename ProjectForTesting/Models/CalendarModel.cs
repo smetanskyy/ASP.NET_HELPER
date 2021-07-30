1e5ebc60463755861d4578f6a623c4cdc77b1244
Stepan Smetanskyi <Stepan.Smetanskyi@campus-support.ca>
2021-07-30 12:09:20 -0700

    added include possibility

---
+++ ProjectForTesting/Models/CalendarModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectForTesting.Models
+{
+    public class CalendarModel
+    {
+        public DateTime Date { get; set; }
+    }
+}
---
+++ ProjectForTesting/Models/ModelTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectForTesting.Models
+{
+    public class ModelTest
+    {
+        public DateTime Date { get; set; }
+    }
+}
